13656216121a29613c487bbd374e62222f82ad12
chloetohce <chloetohce@gmail.com>
2025-01-14 11:22:20 +0800

    Completed

--- src/main/java/paf/lecture/paf_25l/producer/controller/ProducerController.java
+++ src/main/java/paf/lecture/paf_25l/producer/controller/ProducerController.java
@@ -9,8 +9,8 @@ import paf.lecture.paf_25l.producer.service.ProducerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
@@ -19,9 +19,14 @@ public class ProducerController {
     @Autowired
     private ProducerService producerService;
 
-    @PostMapping("")
-    public ResponseEntity<String> sendMessage(@RequestBody Todo todo) {
-        producerService.sendMessage(todo);
+    @GetMapping("")
+    public ResponseEntity<String> sendMessage() {
+        for (int i = 0; i < 10; i++) {
+			Todo todo = new Todo();
+			todo.setId(i);
+			todo.setTask(" " + i);
+			producerService.sendMessage(todo);
+		}
         return new ResponseEntity<>("Message sent.", HttpStatus.OK);
     }
     
